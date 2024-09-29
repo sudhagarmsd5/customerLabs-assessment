3dce2273ba5f937580d911135945c803e3476121
Sudhagar Murugesan <54501573+sudhagarmsd5@users.noreply.github.com>
2024-09-29 13:20:16 +0000

    chore: build changes

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.tsx',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    clean: true,
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src/'),
@@ -27,7 +22,24 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: './src/assets/[name].[ext]',
+              outputPath: 'assets/', 
+            },
+          },
+        ],
+      },
     ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+    clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
