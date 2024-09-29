430eb4c698b800817fd72f14c846d83f537a21cd
Sudhagar Murugesan <54501573+sudhagarmsd5@users.noreply.github.com>
2024-09-29 13:35:49 +0000

    fix: btn click prevent default

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import "./index.css"
 import ViewAudience from '@/pages/Audience/ViewAudience';
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<ViewAudience />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
